27cf3da2b51a780b05957a76240b659ca72650a0
Athar <ansari.athar8@gmail.com>
2021-02-11 01:16:13 +0530

    TeamCityExamples

---
+++ TeamCityExamples_Nuget/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamCityExamples_Nuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewtonJsonExample.WriteJson();
+            Console.ReadLine();
+        }
+    }
+}
